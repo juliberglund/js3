7d42ebe62df3a0e9398b6c35a2703f6d149ad0fc
Julia Berglund <jullano9@hotmail.com>
2024-10-27 10:15:11 +0100

    done with 3 and 4, next step is 5

--- code/3-loops.js
+++ code/3-loops.js
@@ -1,8 +1,16 @@
 // Loops allow you to execute code repeatedly based on a condition.
 
 // Task 1: Write a for loop to print numbers from 1 to 5.
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
 
 // Task 2: Create a while loop that prints the numbers 10 to 15.
+let i = 10;
+while (i < 16) {
+  console.log(i);
+  i++;
+}
 
 // Task 3. Write for-loop that loops through the array `numbers`
 // compare every number with the variable `lowest`.
@@ -10,6 +18,25 @@
 // Use console.log to print `lowest`.
 const numbers = [9, 10, 17, 34, 2, 4, 100];
 let lowest = numbers[0];
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] < lowest) {
+    lowest = numbers[i];
+  }
+}
+console.log(lowest);
 
 // Task 4, Write a function `getLowest` that takes an array of numbers as
 // input and returns the lowest number.
+
+const nbrArray = [6, 8, 9, 4, 3];
+let low = nbrArray[0];
+function getLowest(nbrArray) {
+  for (let i = 1; i < nbrArray.length; i++) {
+    if (nbrArray[i] < low) {
+      low = nbrArray[i];
+    }
+  }
+  return low;
+}
+const lowestNumber = getLowest(nbrArray);
+console.log("Lägsta numret är: " + lowestNumber);
--- code/4-filter.js
+++ code/4-filter.js
@@ -4,8 +4,18 @@ const ages = [12, 18, 25, 8, 30, 16];
 
 // Task 1: Use the .filter() method to create a new array called `adultAges` which only
 // includes ages that are 18 or older. Use console.log to print the `adultAges` array.
+const adultAges = ages.filter((ages) => ages >= 18);
+console.log(adultAges);
 
 // Task 2: Write a function called `getAdultAges` that takes an array of ages as an argument
 // and returns a new array with only the adult ages.
+const getAdultAges = (ages) => {
+  const newArray = ages.filter((ages) => ages >= 18);
+  return newArray;
+};
+const adultAgeNew = getAdultAges(ages);
+console.log(adultAgeNew);
 
 // Task 3: Use the `getAdultAges` function to create a new array called `adultAges`.
+const adultAgesFromFunction = getAdultAges(ages);
+console.log(adultAgesFromFunction);
