a0cbb53de96e02a0639c2e5515df2d0d535d3316
Julia Berglund <jullano9@hotmail.com>
2024-10-29 10:18:47 +0100

    truthiness done next up scope

--- code/5-truthiness.js
+++ code/5-truthiness.js
@@ -3,12 +3,44 @@
 
 // Task 1: Create an array called `values` containing the following values:
 // 0, "hello", null, undefined, false, "false", -1 and "JavaScript".
+const values = [0, "hello", undefined, false, "false", -1, "JavaScript"];
 
 // Task 2: Write a function called `checkTruthiness` that takes a value and
 // logs whether it is truthy or falsy.
 
+const checkTruthiness = (value) => {
+  if (value) {
+    console.log(`${value} is Truthy`);
+  } else {
+    console.log(`${value} is Falsy`);
+  }
+};
+
+checkTruthiness(0);
+checkTruthiness("hello");
+checkTruthiness(null);
+checkTruthiness(undefined);
+checkTruthiness(false);
+checkTruthiness("false");
+checkTruthiness(-1);
+checkTruthiness("JavaScript");
+
 // Task 3: Loop through the `values` array using forEach and call `checkTruthiness` on each value.
+values.forEach((value) => checkTruthiness(value));
 
 // Task 4. Loop through the `values` array using a for-loop and call `checkTruthiness` on each value.
+console.log("ForLoop");
+for (let index = 0; index < values.length; index++) {
+  const element = values[index];
+  console.log(element);
+}
 
 // Task 5. Loop through the `values` array using a while-loop and call `checkTruthiness` on each value.
+console.log("WhileLoop:");
+let index = 0;
+while (index < values.length) {
+  const element = values[index];
+
+  console.log(element);
+  index++;
+}
